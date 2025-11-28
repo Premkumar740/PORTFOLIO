4e539586b6a0207a56c04c2da6b6de843b56cc3c
Premkumar740 <premkumarr740@gmail.com>
2025-11-28 11:39:55 +0530

    Update metadata for author and creator details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,37 +8,38 @@ import { Manropes } from "./fonts";
 import keywords from "./keywords";
 
 export const metadata: Metadata = {
-  title: 'Prem Kumar',
+  title: 'PremKumar',
   description:
     "I'm a student developer who loves building web apps for fun. I enjoy experimenting with technologies to create free, useful tools that everyone can enjoy.",
   authors: [
     {
-      name: 'Balaji',
-      url: 'https://kbalaji-portfolio.netlify.app',
+      name: 'Premkumar',
+      url: 'https://portfolio-ni1e0elzy-premkumar740s-projects.vercel.app',
     },
   ],
-  creator: 'Balaji',
+  creator: 'Premkumar',
   referrer: 'origin-when-cross-origin',
   keywords: keywords,
   openGraph: {
-    title: 'Prem Kumar',
+    title: 'PremKumar',
     description:
       "I'm a student developer who loves building web apps for fun. I enjoy experimenting with technologies to create free, useful tools that everyone can enjoy.",
     images: [{ url: '/images/thumbnail.png' }],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Balaji',
+    title: 'Premkumar',
     description:
       "I'm a student developer who loves building web apps for fun. I enjoy experimenting with technologies to create free, useful tools that everyone can enjoy.",
     images: [{ url: '/images/thumbnail.png' }],
-    creator: '@Balaji',
+    creator: '@Premkumar',
   },
   alternates: {
     canonical: '/',
   },
   icons: '/images/logo.svg',
-  metadataBase: new URL('https://kbalaji-portfolio.netlify.app'),
+  metadataBase: new URL('https://portfolio-ni1e0elzy-premkumar740s-projects.vercel.app'),
+,
 };
 
 export default function RootLayout({
