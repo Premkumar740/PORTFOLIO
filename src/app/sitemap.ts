7b5fd81c8f01265a0113094f35cb92062f25f880
Premkumar740 <premkumarr740@gmail.com>
2025-11-28 12:20:42 +0530

    Update sitemap URL to new project link

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://kbalaji-portfolio.netlify.app",
+      url: "https://portfolio-ni1e0elzy-premkumar740s-projects.vercel.app",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
